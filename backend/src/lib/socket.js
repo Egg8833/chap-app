bacfbf56f25d289bcf042565bdc91297bd235627
黃禎平 <egg8833@gmail.com>
2025-03-07 11:30:20 +0800

    [fix] 移除多餘傳送訊息socket

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -15,10 +15,6 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId]
 }
 
-function isUserChatOnline(userChatId) {
-  return userSocketMap.hasOwnProperty(userChatId)
-}
-
 const userSocketMap = {} // { userId: socketId }
 const activeChats = {} // { userId: chatWithUserId }
 let isBothInChat = false
@@ -34,29 +30,12 @@ io.on('connection', socket => {
 
   socket.on('userInChat', userInChat => {
     const [chatWithUserId, currentUserId] = userInChat // [對方ID, 自己ID]
-    console.log('🔵 userInChat:', {chatWithUserId, currentUserId})
 
-    // **🔹 先通知舊的聊天室對象：「我離開了聊天室」**
-    if (activeChats[currentUserId]) {
-      const previousChatWithUserId = activeChats[currentUserId]
-      const previousRoom = `room-${currentUserId}-${previousChatWithUserId}`
-
-      // 離開舊的房間
-      socket.leave(previousRoom)
-      console.log(`🚪 ${currentUserId} 離開 ${previousRoom}`)
-
-      // 通知還在聊天室的用戶
-      io.to(previousRoom).emit('userLeftChat', currentUserId)
-    }
 
     // **🔹 記錄當前使用者與誰聊天**
     activeChats[currentUserId] = chatWithUserId
     console.log('📌 activeChats:', activeChats)
 
-    // **🔹 設定 room**
-    const room = `room-${currentUserId}-${chatWithUserId}`
-    socket.join(room)
-    console.log(`🏠 ${currentUserId} 加入 ${room}`)
 
     // **🔹 檢查對方是否在線**
     const isOtherUserOnline = chatWithUserId in userSocketMap
@@ -65,12 +44,6 @@ io.on('connection', socket => {
     isBothInChat =
       activeChats[chatWithUserId] === currentUserId &&
       activeChats[currentUserId] === chatWithUserId
-
-    // **🔹 如果雙方都在彼此聊天室，讓對方加入相同的 room**
-    if (isBothInChat && userSocketMap[chatWithUserId]) {
-      io.to(userSocketMap[chatWithUserId]).socketsJoin(room)
-      console.log(`🔗 雙方都在 ${room}`)
-    }
 
     // **🔹 設定聊天室狀態**
     const status = isBothInChat
@@ -104,10 +77,6 @@ io.on('connection', socket => {
     // **🔹 刪除 activeChats 記錄**
     delete activeChats[userId]
 
-    // **🔹 離開 room**
-    const room = `room-${userId}-${chatWithUserId}`
-    socket.leave(room)
-    console.log(`🚪 ${userId} 離開 ${room}`)
 
     // **🔹 如果對方在線，通知對方**
     if (userSocketMap[chatWithUserId]) {
@@ -119,16 +88,8 @@ io.on('connection', socket => {
     console.log('A user disconnected', socket.id)
     delete userSocketMap[userId]
 
-    // **🔹 移除 activeChats 記錄**
-    delete activeChats[userId]
 
-    // **🔹 離開所有聊天室**
-    const rooms = Array.from(socket.rooms).filter(r => r.startsWith('room-'))
-    rooms.forEach(room => {
-      socket.leave(room)
-      io.to(room).emit('userLeftChat', userId)
-      console.log(`🚪 ${userId} 離開 ${room} (disconnect)`)
-    })
+    delete activeChats[userId]
 
     io.emit('getOnlineUsers', Object.keys(userSocketMap))
     console.log('disconnect-userSocketMap', userSocketMap)
